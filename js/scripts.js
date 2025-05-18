f8589dfb42b6da56ef72f7385cfff7f52d23dfc3
Brandon Mamone <148015178+brandonmamone@users.noreply.github.com>
2025-05-17 18:10:40 -0700

    add functionality to the menu in .js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 console.log("Hello, welcome to my portfolio website!");
 
-
+function menuToggle() {
+    var x = document.getElementById ("mynavtoggle");
+    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+} else {
+        x.className = 'navtoggle';
+    }
+}
